112b67034aca0307b3551f9a13bfdf3d13e78a6b
JT Reyes <jesustgreyes@gmail.com>
2023-09-06 12:06:07 -0700

    add tag update/put route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,8 +40,19 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const tagUpdated = await Tag.update(req.body,{
+      where:{
+        id: req.params.id
+      }
+    });
+    res.status(200).json(tagUpdated);
+  }
+  catch(error){
+    res.status(400).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
