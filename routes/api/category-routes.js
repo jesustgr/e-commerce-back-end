ac31b7962702fc30472efc0f2dbee3e2e76e3269
JT Reyes <jesustgreyes@gmail.com>
2023-09-06 12:11:21 -0700

    add catefory update/put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,8 +42,19 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try{
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updatedCategory);
+  }
+  catch (error){
+    res.status(400).send(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
