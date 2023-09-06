ff871961b8e0bfa95e10d34328018669dc014864
JT Reyes <jesustgreyes@gmail.com>
2023-09-06 12:14:59 -0700

    sync sequelize models to database

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+(async () => {
+  try {
+    await sequelize.sync({ force: false });
+    console.log('Sequelize models are synced to the database.');
+
+    app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+  } catch (error) {
+    console.error('Cannot sync Sequelize models to the database:', error);
+  }
+})();
