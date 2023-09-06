1ab9d53edec8b04dabe3a2d02212051171a2dbad
JT Reyes <jesustgreyes@gmail.com>
2023-09-06 12:03:44 -0700

    add tag get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try{
+    const tagData = await Tag.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(tagData);
+  }
+  catch (error){
+    res.status(400).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try{
+    const tagById = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    res.status(200).json(tagById);
+  }
+  catch (error){
+    res.status(400).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
